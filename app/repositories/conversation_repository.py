472f8b6c97fde52cbe501cce0973467131112e25
Zagan Alexandru <alexzagann@gmail.com>
2025-12-04 14:18:17 +0200

    added other_participant_id

--- app/repositories/conversation_repository.py
+++ app/repositories/conversation_repository.py
@@ -27,10 +27,23 @@ def get_user_conversations_with_last_message_and_unread_count(db: Session, user_
                 Message.sent_at > participant.last_read_at
             )
         ).count()
+        # identifică id-ul userului cu care conversezi (altul decât user_id)
+        other_participant_id = None
+        if conv.is_group:
+            # pentru grup, nu există un singur "celălalt" user
+            other_participant_id = None
+        else:
+            # pentru conversație 1-la-1, găsește celălalt user
+            other = db.query(ConversationParticipant).filter(
+                ConversationParticipant.conversation_id == conv.conversation_id,
+                ConversationParticipant.user_id != user_id
+            ).first()
+            if other:
+                other_participant_id = other.user_id
         result.append({
             'conversation': conv,
             'last_message': last_msg,
             'unread_count': unread_count,
-            'user_id': user_id
+            'other_user_id': other_participant_id
         })
     return result
